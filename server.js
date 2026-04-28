04afad08009608dad575f19db7bde5de70fd6e98
elisalindstrom <elisalindstroem@gmail.com>
2026-04-28 21:32:08 +0200

    Skapat schema med mongoose, testat hämta och lägga till dokument i databasen

--- server.js
+++ server.js
@@ -8,8 +8,48 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/cv"). then(() => {
+// Databaskoppling
+mongoose.connect("mongodb://localhost:27017/cv").then(() => {
     console.log("Connected to Mongodb");
 }).catch((error) => {
     console.log("Error connecting to db:" + error);
+})
+
+// Schema
+const WorkexperienceSchema = new mongoose.Schema({
+    companyname: { type: String, required: [true, "Fyll i företagets namn"] },
+    jobtitle: { type: String, required: [true, "Fyll i jobbtitel"] },
+    startdate: { type: Date, required: [true, "Fyll i startdatum"] },
+    enddate: { type: Date }
+})
+
+const Workexperience = mongoose.model("Workexperience", WorkexperienceSchema);
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("API running");
+})
+
+app.get("/workexperience", async (req, res) => {
+    try {
+        let result = await Workexperience.find({});
+
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+})
+
+app.post("/workexperience", async (req, res) => {
+    try {
+        let result = await Workexperience.create(req.body);
+
+        return res.json(result);
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+})
+
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:" + port);
 })
